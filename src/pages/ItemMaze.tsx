d1e3ab256e23f77279ebea688134469d88f5b293
Tamkish <martin@vrba.dev>
2023-02-26 21:40:38 +0100

    cringe ItemMaze made slightly less cringe

--- src/pages/ItemMaze.tsx
+++ src/pages/ItemMaze.tsx
@@ -1,28 +1,28 @@
 import {FC, useEffect, useState} from "react";
 
-type RoomCode =
+type RoomName =
     "bone" | "mail" | "banana" | "cactus" |
     "cheese" | "carrot" | "mushroom" |
     "flower" | "tree" | "bread" | "rock" | "sparkles"
 
 
 interface Room {
-    id: RoomCode;
+    id: RoomName;
     title: string;
     connections: Connection[];
 }
 
 type Connection = {
-    to: RoomCode;
-    lock: LockCode | null;
+    to: RoomName;
+    lock: LockName | null;
 }
 
-type LockCode = "monkey" | "rabbit" | "dog" | "mouse" | "duck"
+type LockName = "monkey" | "rabbit" | "dog" | "mouse" | "duck"
 
 type Lock = {
-    id: LockCode;
+    id: LockName;
     title: string;
-    unlockedBy: RoomCode;
+    unlockedBy: RoomName;
 }
 
 
@@ -103,7 +103,7 @@ const rooms: Room[] = [
         title: "Kytka",
         connections: [
             {lock: "dog", to: "bread"},
-            {lock: null, to: "cactus"},
+            {lock: null, to: "rock"},
         ]
 
     },
@@ -186,7 +186,7 @@ const locks: Lock[] = [
 ]
 
 
-const emojis =
+/*const emojis =
     {
         bone: "🦴",
         mail: "✉",
@@ -211,60 +211,124 @@ const emojis =
         door: "🚪",
         no: "❌",
         yes: "✔"
-    }
+    }*/
 
-type MiscCode = "bag" | "lock" | "find" | "door" | "no" | "yes";
+const emojis = {
+    bone: "1F9B4",
+    mail: "2709",
+    banana: "1F34C",
+    cactus: "1F335",
+    cheese: "1F9C0",
+    carrot: "1F955",
+    mushroom: "1F344",
+    flower: "1F33C",
+    tree: "1F332",
+    bread: "1F950",
+    rock: "1FAA8",
+    sparkles: "2728",
+    monkey: "1F435",
+    rabbit: "1F430",
+    dog: "1F436",
+    mouse: "1F42D",
+    duck: "1F986",
+    bag: "1F4BC",
+    door: "1F6AA",
+    no: "274C",
+    yes: "2714",
+    think: "1F4AD",
+    question: "2754",
+}
+type MiscName = "bag" | "door" | "no" | "yes" | "think" | "question";
+type EmojiName = RoomName | LockName | MiscName;
+const getEmojiUrl = (name: EmojiName) => {
+    const openmoji = `https://openmoji.org/data/color/svg/`//code.toUpperCase()
+    const twemoji = `https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/`//code.toLowerCase()
 
-type EmojiCode = RoomCode | LockCode | MiscCode;
+    const code = emojis[name].toUpperCase();
+    return openmoji + code + ".svg";
+
+}
+
+const getRoom = (name: RoomName) => {
+    return rooms.find(room => room.id == name)!
+}
 
 
 const ItemMaze = () => {
+    const initialVisitedRooms: RoomName[] = ["sparkles"];
+    const defaultThinkingIcon: EmojiName = "think";
+    const maxRooms = 12;
 
-    const [unicode, setUnicode] = useState<boolean>(false)
-    const [visitedRooms, setVisitedRooms] = useState<RoomCode[]>([]);
+    const [visitedRooms, setVisitedRooms] = useState<RoomName[]>(initialVisitedRooms);
+    const [thinkingIcon, setThinkingIcon] = useState<EmojiName>(defaultThinkingIcon)
+    const [currentConnections, setCurrentConnections] = useState<Connection[]>([])
 
-    const addRoom = (roomCode: RoomCode) => {
+    useEffect(() => reset(), [])
+
+    const addRoom = (roomCode: RoomName) => {
         setVisitedRooms([...visitedRooms, roomCode])
+        //https://stackoverflow.com/a/46545530
+        setCurrentConnections(
+            getRoom(roomCode).connections.sort(() => .5 - Math.random())
+        )
+
     }
 
-    const hasVisited = (roomCode: RoomCode) => {
+    const hasVisited = (roomCode: RoomName) => {
         return visitedRooms.indexOf(roomCode) != -1;
     }
 
-    const neededKey = (lockCode: LockCode) => {
+    const neededKey = (lockCode: LockName) => {
         const lock = locks.find(l => l.id == lockCode);
         return lock!.unlockedBy;
     }
 
 
     type EmojiProps = {
-        code: EmojiCode,
+        code: EmojiName,
     }
     const Emoji: FC<EmojiProps> = ({code}: EmojiProps) => {
+        const unicodeEmoji = String.fromCodePoint(parseInt(emojis[code], 16))
+
         return (
-            <div className={"inline"}>
+            <div className={"h-10 w-10 inline"}>
                 {
-                    unicode
+
+                    /*unicode
                         ? <span className={"text-4xl"}>{emojis[code]}</span>
-                        : <img alt={emojis[code]} className={"h-10 inline align-top"} src={`/emojis/${code}.svg`}/>
+                        :*/
+                    <img alt={unicodeEmoji} className={"h-10 inline align-top"} src={getEmojiUrl(code)}/>
                 }
             </div>
 
         )
     }
     const Bag: FC = () => {
+
+        let displayedEmojis: EmojiName[] = [...visitedRooms];
+        for (let i = visitedRooms.length; i < maxRooms; i++) {
+            displayedEmojis.push("question");
+        }
+
         return (
-            <div>
-                <div className={"bg-indigo-300 inline-block m-4 rounded-xl p-2 min-w-fit"}>
-                    <div className={"inline-block mr-10 ml-2"}>
+            <div className={"flex flex-row"}>
+                <div className={""}>
+                    <div className={""}>
                         <Emoji code={"bag"}/>
                     </div>
-                    <div className={"inline-block"}>
-
-                        {
-                            visitedRooms.map((roomCode, index) => <Emoji key={index} code={roomCode}/>)
-                        }
+                    <div>
+                        <Emoji code={thinkingIcon}/>
                     </div>
+
+                    <div className={"bg-green-300"}>
+                        <Emoji code={visitedRooms.at(-1)!}/>
+                    </div>
+                </div>
+                <div className={"bg-indigo-300 grid grid-cols-4"}>
+
+                    {
+                        displayedEmojis.map((roomCode, index) => <Emoji key={index} code={roomCode}/>)
+                    }
                 </div>
             </div>
         )
@@ -272,21 +336,23 @@ const ItemMaze = () => {
 
     const Doors: FC = () => {
         if (visitedRooms.length == 0) return <></>;
-        const lastVisitedRoom: RoomCode = visitedRooms.at(-1)!;
-        const currentRoom: Room = rooms.find(searchedRoom => searchedRoom.id == lastVisitedRoom)!
+        if (visitedRooms.length >= maxRooms) return <></>
 
-        //https://stackoverflow.com/a/46545530
-        const shuffledConnections = currentRoom.connections.sort(() => .5 - Math.random());
+        //const lastVisitedRoom: RoomName = visitedRooms.at(-1)!;
+        //const currentRoom: Room = rooms.find(searchedRoom => searchedRoom.id == lastVisitedRoom)!
 
         type DoorProps = {
             enabled: boolean,
-            emoji: EmojiCode | null,
-            linkedTo: RoomCode,
+            emoji: EmojiName | null,
+            linkedTo: RoomName,
+            thinkingItem: EmojiName,
         }
-        const Door: FC<DoorProps> = ({enabled, emoji, linkedTo}) => {
+        const Door: FC<DoorProps> = ({enabled, emoji, linkedTo, thinkingItem}) => {
 
             const [hasMouse, setHasMouse] = useState<boolean>(false)
-            const shownEmoji: EmojiCode =
+
+
+            const shownEmoji: EmojiName =
                 hasMouse
                     ? enabled ? linkedTo : "no"
                     : emoji ?? "door";
@@ -296,8 +362,15 @@ const ItemMaze = () => {
                         cursor: enabled ? "pointer" : "not-allowed"
                     }}
                     className={"inline-block m-1 rounded-xl min-w-fit"}
-                    onMouseEnter={() => setHasMouse(true)}
-                    onMouseLeave={() => setHasMouse(false)}
+                    onMouseEnter={() => {
+                        setHasMouse(true);
+                        if (thinkingItem) setThinkingIcon(thinkingItem)
+
+                    }}
+                    onMouseLeave={() => {
+                        setHasMouse(false)
+                        setThinkingIcon(defaultThinkingIcon)
+                    }}
                 >
                     <a onClick={() => {
                         if (enabled) addRoom(linkedTo);
@@ -311,16 +384,18 @@ const ItemMaze = () => {
 
         return (
             <div>
-
-                <div className={"m-2 inline-block m-4 rounded-xl p-2 min-w-fit bg-amber-500"}>
+                <div className={"bg-amber-500"}>
                     {
-
-                        shuffledConnections.map((connection, index) => {
+                        currentConnections.map((connection, index) => {
                                 const canEnter =
                                     connection.lock == null
                                         ? true
                                         : hasVisited(neededKey(connection.lock))
 
+                                const missingItem =
+                                    canEnter
+                                        ? "yes"
+                                        : neededKey(connection.lock!);
 
                                 return (
                                     <Door
@@ -328,6 +403,7 @@ const ItemMaze = () => {
                                         enabled={canEnter}
                                         emoji={connection.lock}
                                         linkedTo={connection.to}
+                                        thinkingItem={missingItem}
                                     />
 
                                 )
@@ -345,14 +421,14 @@ const ItemMaze = () => {
         return (<>
                 <div className={"text-gray-400 text-sm m-2"}>
                     <button
-                        className={"text-black text-lg font-bold rounded bg-gray-300 px-2 py-1 hover:bg-gray-400 active:bg-gray-500"}
+                        className={"block text-black text-lg font-bold rounded bg-gray-300 px-2 py-1 hover:bg-gray-400 active:bg-gray-500"}
                         onClick={reset}> Resetovat
                     </button>
-                    <div>
+                    {/*<div>
                         <input className={"m-1"} type={"checkbox"} defaultChecked={unicode}
                                onChange={(event) => setUnicode(event.target.checked)}/>
                         Použít unicode
-                    </div>
+                    </div>*/}
                     Stránka používá <a href={"https://openmoji.org"}>OpenMoji</a>. Licence: <a
                     href={"https://creativecommons.org/licenses/by-sa/4.0/#"}>CC BY-SA 4.0</a>
                 </div>
@@ -360,20 +436,28 @@ const ItemMaze = () => {
         )
     }
 
-    const reset = () => setVisitedRooms(["sparkles"]);
-    useEffect(() => {
+    const reset = () => {
+        setVisitedRooms(initialVisitedRooms);
+        setCurrentConnections(getRoom("sparkles").connections.sort(() => .5 - Math.random()))
+    };
+    /*useEffect(() => {
         reset();
-    }, [])
+    }, [])*/
 
 
     return (
-        <div className={"text-4xl"}>
-            <Bag/>
-            {
-                visitedRooms.length > 0 &&
-                <Doors/>
-            }
-            <Footnote/>
+        <div className={"text-4xl flex h-screen"}>
+            <div className={"m-auto bg-gray-100 p-1"}>
+
+                {/*<Twemoji options={{folder: "svg", ext: ".svg"}}>*/}
+
+                <Bag/>
+                {
+                    visitedRooms.length > 0 &&
+                    <Doors/>
+                }
+                <Footnote/>
+            </div>
         </div>
     )
 }

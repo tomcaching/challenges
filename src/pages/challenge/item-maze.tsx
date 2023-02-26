9ed57d60f5504902e7d91cf499b7cc44c1d559dd
Jiří Vrba <jiri@vrba.dev>
2023-02-26 22:02:14 +0100

    Extract nested components (big performance no no)

--- src/pages/challenge/item-maze.tsx
+++ src/pages/challenge/item-maze.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect, useState} from "react";
+import React, { type FC, useEffect, useState } from "react";
 
 type RoomName =
     "bone" | "mail" | "banana" | "cactus" |
@@ -25,7 +25,6 @@ type Lock = {
     unlockedBy: RoomName;
 }
 
-
 const rooms: Room[] = [
 
     //kost:
@@ -33,8 +32,8 @@ const rooms: Room[] = [
         id: "bone",
         title: "Kost",
         connections: [
-            {lock: null, to: "flower"},
-            {lock: "rabbit", to: "bread"},
+            { lock: null, to: "flower" },
+            { lock: "rabbit", to: "bread" },
         ]
     },
     //konec:
@@ -48,9 +47,9 @@ const rooms: Room[] = [
         id: "banana",
         title: "",
         connections: [
-            {lock: "duck", to: "tree"},
-            {lock: null, to: "bone"},
-            {lock: "rabbit", to: "flower"},
+            { lock: "duck", to: "tree" },
+            { lock: null, to: "bone" },
+            { lock: "rabbit", to: "flower" },
         ]
 
     },
@@ -59,8 +58,8 @@ const rooms: Room[] = [
         id: "cactus",
         title: "Kaktus",
         connections: [
-            {lock: "mouse", to: "mail"},
-            {lock: null, to: "carrot"},
+            { lock: "mouse", to: "mail" },
+            { lock: null, to: "carrot" },
         ]
 
     },
@@ -69,9 +68,9 @@ const rooms: Room[] = [
         id: "cheese",
         title: "Sýr",
         connections: [
-            {lock: null, to: "bread"},
-            {lock: "duck", to: "carrot"},
-            {lock: null, to: "flower"},
+            { lock: null, to: "bread" },
+            { lock: "duck", to: "carrot" },
+            { lock: null, to: "flower" },
         ]
 
     },
@@ -80,8 +79,8 @@ const rooms: Room[] = [
         id: "carrot",
         title: "Mrkev",
         connections: [
-            {lock: "dog", to: "banana"},
-            {lock: null, to: "rock"},
+            { lock: "dog", to: "banana" },
+            { lock: null, to: "rock" },
         ]
 
     },
@@ -90,10 +89,10 @@ const rooms: Room[] = [
         id: "mushroom",
         title: "Houba",
         connections: [
-            {lock: null, to: "flower"},
-            {lock: "monkey", to: "cactus"},
-            {lock: "mouse", to: "mail"},
-            {lock: "rabbit", to: "rock"},
+            { lock: null, to: "flower" },
+            { lock: "monkey", to: "cactus" },
+            { lock: "mouse", to: "mail" },
+            { lock: "rabbit", to: "rock" },
         ]
 
     },
@@ -102,8 +101,8 @@ const rooms: Room[] = [
         id: "flower",
         title: "Kytka",
         connections: [
-            {lock: "dog", to: "bread"},
-            {lock: null, to: "rock"},
+            { lock: "dog", to: "bread" },
+            { lock: null, to: "rock" },
         ]
 
     },
@@ -112,9 +111,9 @@ const rooms: Room[] = [
         id: "tree",
         title: "Strom",
         connections: [
-            {lock: null, to: "bone"},
-            {lock: "monkey", to: "rock"},
-            {lock: "duck", to: "mushroom"},
+            { lock: null, to: "bone" },
+            { lock: "monkey", to: "rock" },
+            { lock: "duck", to: "mushroom" },
         ]
 
     },
@@ -123,8 +122,8 @@ const rooms: Room[] = [
         id: "bread",
         title: "Chleba",
         connections: [
-            {lock: null, to: "cheese"},
-            {lock: "dog", to: "carrot"},
+            { lock: null, to: "cheese" },
+            { lock: "dog", to: "carrot" },
         ]
     },
     //kamen:
@@ -132,11 +131,11 @@ const rooms: Room[] = [
         id: "rock",
         title: "Kámen",
         connections: [
-            {lock: "monkey", to: "cactus"},
-            {lock: null, to: "banana"},
-            {lock: null, to: "bread"},
-            {lock: "dog", to: "flower"},
-            {lock: null, to: "bone"},
+            { lock: "monkey", to: "cactus" },
+            { lock: null, to: "banana" },
+            { lock: null, to: "bread" },
+            { lock: "dog", to: "flower" },
+            { lock: null, to: "bone" },
         ]
     },
     //start:
@@ -144,8 +143,8 @@ const rooms: Room[] = [
         id: "sparkles",
         title: "Zacatek",
         connections: [
-            {lock: null, to: "rock"},
-            {lock: null, to: "mushroom"},
+            { lock: null, to: "rock" },
+            { lock: null, to: "mushroom" },
         ]
     },
 
@@ -240,12 +239,14 @@ const emojis = {
 }
 type MiscName = "bag" | "door" | "no" | "yes" | "think" | "question";
 type EmojiName = RoomName | LockName | MiscName;
+
 const getEmojiUrl = (name: EmojiName) => {
     const openmoji = `https://openmoji.org/data/color/svg/`//code.toUpperCase()
     const twemoji = `https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/svg/`//code.toLowerCase()
 
-    const code = emojis[name].toUpperCase();
-    return openmoji + code + ".svg";
+    const code = emojis[name].toLowerCase();
+
+    return twemoji + code + ".svg";
 
 }
 
@@ -253,6 +254,79 @@ const getRoom = (name: RoomName) => {
     return rooms.find(room => room.id == name)!
 }
 
+type EmojiProps = {
+    code: EmojiName,
+}
+
+const Emoji: FC<EmojiProps> = ({ code }: EmojiProps) => {
+    const unicodeEmoji = String.fromCodePoint(parseInt(emojis[code], 16))
+
+    return (
+        <div className={"h-10 w-10 inline"}>
+            {
+
+                /*unicode
+                    ? <span className={"text-4xl"}>{emojis[code]}</span>
+                    :*/
+                <img alt={unicodeEmoji} className={"h-10 inline align-top"} src={getEmojiUrl(code)} />
+            }
+        </div>
+    );
+}
+
+type BagProps = {
+    visitedRooms: RoomName[];
+    maxRooms: number;
+    thinkingIcon: EmojiName;
+}
+
+const Bag: FC<BagProps> = ({ visitedRooms, maxRooms, thinkingIcon }) => {
+    let displayedEmojis: EmojiName[] = [...visitedRooms];
+
+    for (let i = visitedRooms.length; i < maxRooms; i++) {
+        displayedEmojis.push("question");
+    }
+
+    return (
+        <div className={"flex flex-row"}>
+            <div className={""}>
+                <div className={""}>
+                    <Emoji code={"bag"} />
+                </div>
+                <div>
+                    <Emoji code={thinkingIcon} />
+                </div>
+
+                <div className={"bg-green-300"}>
+                    <Emoji code={visitedRooms.at(-1)!} />
+                </div>
+            </div>
+            <div className={"bg-indigo-300 grid grid-cols-4"}>
+
+                {
+                    displayedEmojis.map((roomCode, index) => <Emoji key={index} code={roomCode} />)
+                }
+            </div>
+        </div>
+    )
+}
+
+
+type FootnoteProps = {
+    reset: () => void;
+}
+
+const Footnote: FC<FootnoteProps> = ({ reset }) => {
+    return (<>
+        <div className={"text-gray-400 text-sm m-2"}>
+            <button
+                className={"block text-black text-lg font-bold rounded bg-gray-300 px-2 py-1 hover:bg-gray-400 active:bg-gray-500"}
+                onClick={reset}> Resetovat
+            </button>
+        </div>
+    </>
+    )
+};
 
 const ItemMaze = () => {
     const initialVisitedRooms: RoomName[] = ["sparkles"];
@@ -268,10 +342,7 @@ const ItemMaze = () => {
     const addRoom = (roomCode: RoomName) => {
         setVisitedRooms([...visitedRooms, roomCode])
         //https://stackoverflow.com/a/46545530
-        setCurrentConnections(
-            getRoom(roomCode).connections.sort(() => .5 - Math.random())
-        )
-
+        setCurrentConnections(getRoom(roomCode).connections.sort(() => .5 - Math.random()));
     }
 
     const hasVisited = (roomCode: RoomName) => {
@@ -281,57 +352,6 @@ const ItemMaze = () => {
     const neededKey = (lockCode: LockName) => {
         const lock = locks.find(l => l.id == lockCode);
         return lock!.unlockedBy;
-    }
-
-
-    type EmojiProps = {
-        code: EmojiName,
-    }
-    const Emoji: FC<EmojiProps> = ({code}: EmojiProps) => {
-        const unicodeEmoji = String.fromCodePoint(parseInt(emojis[code], 16))
-
-        return (
-            <div className={"h-10 w-10 inline"}>
-                {
-
-                    /*unicode
-                        ? <span className={"text-4xl"}>{emojis[code]}</span>
-                        :*/
-                    <img alt={unicodeEmoji} className={"h-10 inline align-top"} src={getEmojiUrl(code)}/>
-                }
-            </div>
-
-        )
-    }
-    const Bag: FC = () => {
-
-        let displayedEmojis: EmojiName[] = [...visitedRooms];
-        for (let i = visitedRooms.length; i < maxRooms; i++) {
-            displayedEmojis.push("question");
-        }
-
-        return (
-            <div className={"flex flex-row"}>
-                <div className={""}>
-                    <div className={""}>
-                        <Emoji code={"bag"}/>
-                    </div>
-                    <div>
-                        <Emoji code={thinkingIcon}/>
-                    </div>
-
-                    <div className={"bg-green-300"}>
-                        <Emoji code={visitedRooms.at(-1)!}/>
-                    </div>
-                </div>
-                <div className={"bg-indigo-300 grid grid-cols-4"}>
-
-                    {
-                        displayedEmojis.map((roomCode, index) => <Emoji key={index} code={roomCode}/>)
-                    }
-                </div>
-            </div>
-        )
     }
 
     const Doors: FC = () => {
@@ -347,7 +367,7 @@ const ItemMaze = () => {
             linkedTo: RoomName,
             thinkingItem: EmojiName,
         }
-        const Door: FC<DoorProps> = ({enabled, emoji, linkedTo, thinkingItem}) => {
+        const Door: FC<DoorProps> = ({ enabled, emoji, linkedTo, thinkingItem }) => {
 
             const [hasMouse, setHasMouse] = useState<boolean>(false)
 
@@ -375,7 +395,7 @@ const ItemMaze = () => {
                     <a onClick={() => {
                         if (enabled) addRoom(linkedTo);
                     }}>
-                        <Emoji code={shownEmoji}/>
+                        <Emoji code={shownEmoji} />
                     </a>
                 </div>
             )
@@ -387,27 +407,27 @@ const ItemMaze = () => {
                 <div className={"bg-amber-500"}>
                     {
                         currentConnections.map((connection, index) => {
-                                const canEnter =
-                                    connection.lock == null
-                                        ? true
-                                        : hasVisited(neededKey(connection.lock))
+                            const canEnter =
+                                connection.lock == null
+                                    ? true
+                                    : hasVisited(neededKey(connection.lock))
 
-                                const missingItem =
-                                    canEnter
-                                        ? "yes"
-                                        : neededKey(connection.lock!);
+                            const missingItem =
+                                canEnter
+                                    ? "yes"
+                                    : neededKey(connection.lock!);
 
-                                return (
-                                    <Door
-                                        key={index}
-                                        enabled={canEnter}
-                                        emoji={connection.lock}
-                                        linkedTo={connection.to}
-                                        thinkingItem={missingItem}
-                                    />
+                            return (
+                                <Door
+                                    key={index}
+                                    enabled={canEnter}
+                                    emoji={connection.lock}
+                                    linkedTo={connection.to}
+                                    thinkingItem={missingItem}
+                                />
 
-                                )
-                            }
+                            )
+                        }
                         )
                     }
 
@@ -417,46 +437,20 @@ const ItemMaze = () => {
         )
     }
 
-    const Footnote: FC = () => {
-        return (<>
-                <div className={"text-gray-400 text-sm m-2"}>
-                    <button
-                        className={"block text-black text-lg font-bold rounded bg-gray-300 px-2 py-1 hover:bg-gray-400 active:bg-gray-500"}
-                        onClick={reset}> Resetovat
-                    </button>
-                    {/*<div>
-                        <input className={"m-1"} type={"checkbox"} defaultChecked={unicode}
-                               onChange={(event) => setUnicode(event.target.checked)}/>
-                        Použít unicode
-                    </div>*/}
-                    Stránka používá <a href={"https://openmoji.org"}>OpenMoji</a>. Licence: <a
-                    href={"https://creativecommons.org/licenses/by-sa/4.0/#"}>CC BY-SA 4.0</a>
-                </div>
-            </>
-        )
-    }
-
     const reset = () => {
         setVisitedRooms(initialVisitedRooms);
-        setCurrentConnections(getRoom("sparkles").connections.sort(() => .5 - Math.random()))
+        setCurrentConnections(getRoom("sparkles").connections.sort(() => 0.5 - Math.random()))
     };
-    /*useEffect(() => {
-        reset();
-    }, [])*/
-
 
     return (
         <div className={"text-4xl flex h-screen"}>
             <div className={"m-auto bg-gray-100 p-1"}>
-
-                {/*<Twemoji options={{folder: "svg", ext: ".svg"}}>*/}
-
-                <Bag/>
+                <Bag thinkingIcon={thinkingIcon} visitedRooms={visitedRooms} maxRooms={maxRooms} />
                 {
                     visitedRooms.length > 0 &&
-                    <Doors/>
+                    <Doors />
                 }
-                <Footnote/>
+                <Footnote reset={reset} />
             </div>
         </div>
     )
